27e0070078bae4b12e938cc3603cb608d1bddc27
Ahmad Supriyanto <ahmadsupri1999@gmail.com>
2018-10-17 23:58:35 +0700

    program done but checkbox and every list program reg

--- ProgramApp/forms.py
+++ ProgramApp/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class program_registration_form(forms.Form):
-    nama = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField()
-    jumlah_uang = forms.IntegerField()
+    nama = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control shadow-box-form', 'placeholder': 'Isi nama kamu di sini...'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control shadow-box-form', 'placeholder': 'Isi email kamu di sini...'}))
+    jumlah_uang = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control shadow-box-form', 'placeholder': 'Isi password kamu di sini...'}))
--- ProgramApp/models.py
+++ ProgramApp/models.py
@@ -6,3 +6,9 @@ class program_registration(models.Model):
     nama = models.CharField(max_length=255)
     email = models.EmailField(max_length=100)
     jumlah_uang = models.IntegerField()
+
+
+class program_update(models.Model):
+    judul = models.CharField(max_length=50)
+    konten = models.TextField(max_length=500)
+    image = models.CharField(max_length=200)
