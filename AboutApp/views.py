f891a5916aa843a8f74c2179c82d2d7bf40aa141
Edward Partogi Gembira Abyatar <edwsibr@gmail.com>
2018-12-06 14:45:52 +0700

    fixing models, testimonials not using ajax yet

--- AboutApp/views.py
+++ AboutApp/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .forms import testi_form
 from .models import testi_Model
 
 # Create your views here.
 
 def aboutIndex(request):
+    allTesti = testi_Model.objects.all()
     if (request.method == "POST"):
         form = testi_form(request.POST)
         if (form.is_valid()):
             name = request.POST['name']
             komentar = request.POST['komentar']
-            testi = testi_Model(name = name, komentar = komentar)
+            testi = testi_Model(name = name, komenModel = komentar)
             testi.save()
-            return redirect('/about/')
-    else:
-        form = testi_form()
-        if 'name' in request.session:
-            form = testi_form(initial={'name':request.session['name']})
-    return render(request, 'about.html', {'form':form})
+            return HttpResponseRedirect('/about/')
+    form = testi_form()
+    if 'name' in request.session:
+        form = testi_form(initial={'name':request.session['name']})
+    return render(request, 'about.html', {'form':form, 'allTesti':allTesti})
